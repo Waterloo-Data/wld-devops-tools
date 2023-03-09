6ecdae4b6cd7d96c948041b8f6565c6335b19780
Miller Hooks <Miller Hooks>
2023-03-09 12:14:42 -0600

    correct path in script for kubeflow server build

--- scripts/build-kf-workshop-notebooks.sh
+++ scripts/build-kf-workshop-notebooks.sh
@@ -1,1 +1,3 @@
+cd /godata/pipelines/Kubeflow-Custom-Notebook/kubeflow/components/example-notebook-servers/jupyter-pytorch-full
+
 docker build -t wldsh/jupyter-roberta:v0.4.0 --build-arg BASE_IMG=public.ecr.aws/j1r0q0g6/notebooks/notebook-servers/jupyter-pytorch-cuda-full:v1.5.0 -f cuda.Dockerfile .
